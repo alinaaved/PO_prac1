48e04f3d852b282852c9d305b6c906860128bcbf
alinaaved <alinangs@mail.ru>
2025-03-17 09:55:44 +0300

    update engine.py

--- engine.py
+++ engine.py
@@ -1,27 +1,30 @@
-def run_game(game_module):
+def get_player_name():
     print("Welcome to the Brain Games!")
     try:
-        name = input("May I have your name? ")
+        return input("May I have your name? ")
     except UnicodeDecodeError:
-        name = input("May I have your name? ")
+        return input("May I have your name? ").encode('latin-1').decode('latin-1')
 
-    print(f"Hello, {name}!")
-
+def play_rounds(game_module, name):
     print(game_module.DESCRIPTION)
 
     for _ in range(3):  # Максимум 3 раунда
         question, correct_answer = game_module.generate_question()
         print(f"Question: {question}")
-        try:
-            user_answer = input("Your answer: ")
-        except UnicodeDecodeError:
-            user_answer = input("Your answer: ")
+        user_answer = input("Your answer: ")
 
         if user_answer == correct_answer:
             print("Correct!")
         else:
             print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
-            return
+            return False
 
-    print(f"Congratulations, {name}!")
+    return True
+
+def run_game(game_module):
+    name = get_player_name()
+    print(f"Hello, {name}!")
+
+    if play_rounds(game_module, name):
+        print(f"Congratulations, {name}!")
